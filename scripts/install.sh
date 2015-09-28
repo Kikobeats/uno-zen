92ba10b4086ddbf37338153d1a0ac6bf271412b3
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-09-28 17:04:35 +0200

    first commit

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Cloning repository"
+git clone git@github.com:Kikobeats/uno-zen.git
+
+echo "Getting the repository tagged commits"
+git fetch --tags
+
+echo "Resolving the latest tagged version"
+latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+
+echo "Creating a stable branch from the latest tagged version"
+git checkout "$latestTag"
+git checkout -b stable
