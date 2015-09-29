cc04a441d9355bdf00901eab415c866da1a3854d
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-09-29 11:33:28 +0200

    improve scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d "uno-zen" ]; then
+  echo "\nUno Zen is already installed. Maybe you want to update? Run:"
+  echo "\nsh scripts/update.sh"
+  exit
+fi
 
 git clone https://github.com/Kikobeats/uno-zen.git && cd uno-zen
 
@@ -17,4 +22,4 @@ echo "Creating a stable branch from the latest tagged version"
 git checkout "$latestTag"
 git checkout -b stable
 
-echo "Theme Installed Successful! Enjoy :-)"
+echo "\nTheme Installed Successful! Enjoy :-)"
--- scripts/update.sh
+++ scripts/update.sh
@@ -4,18 +4,16 @@
 
 welcome
 
-currentVersion=$(git describe)
-echo " Your current version is $currentVersion."
+currentVersion=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+echo "Current version: $currentVersion."
 
-echo " Searching for updates..."
+echo "Searching for updates..."
 git fetch --tags origin
 latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-if [ "$currentVersion" == "$latestTag" ]
-then
-  echo "\n You already have the latest version."
+if [ "$currentVersion" == "$latestTag" ]; then
+  echo "\nYou already have the latest version."
 else
-  echo " Updating $currentVersion to $latestTag..."
+  echo "\nUpdating $currentVersion to $latestTag..."
   git merge "$latestTag"
-
 fi
